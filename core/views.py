e69324615280c14e2a2ed3f0bc0096c515a4b993
Bianca Henderson <bianca@redhat.com>
2025-07-01 16:30:00 -0400

    Additionally subclass CoreViewSet in TaskViewSet

--- core/views.py
+++ core/views.py
@@ -81,7 +81,7 @@ class AutomationViewSet(CoreViewSet, ModelViewSet):
     serializer_class = AutomationSerializer
 
 
-class TaskViewSet(ReadOnlyModelViewSet):
+class TaskViewSet(CoreViewSet, ReadOnlyModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
