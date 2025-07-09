a38560bf9b0da7004dacd50be8f5bc43f2b2f64f
Alina Buzachis <abuzachis@redhat.com>
2025-07-09 17:53:46 +0200

    Fix tests
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -87,6 +87,49 @@ class TaskViewSetTest(SharedDataMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
+class PatternViewSetTest(SharedDataMixin, APITestCase):
+    def test_pattern_list_view(self):
+        url = reverse("pattern-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["pattern_name"], "example_pattern")
+
+    def test_pattern_detail_view(self):
+        url = reverse("pattern-detail", args=[self.pattern.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["collection_name"], "mynamespace.mycollection")
+    
+
+    @patch("core.views.async_to_sync")
+    def test_pattern_create_view(self, mock_async_to_sync):
+        url = reverse("pattern-list")
+        data = {
+            "collection_name": "new.namespace.collection",
+            "collection_version": "1.2.3",
+            "collection_version_uri": "https://example.com/new.tar.gz",
+            "pattern_name": "new_pattern",
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        # Pattern created
+        pattern = Pattern.objects.get(pattern_name="new_pattern")
+        self.assertIsNotNone(pattern)
+
+        # Task id returned directly
+        task_id = response.data.get("task_id")
+        self.assertIsInstance(task_id, int)
+
+        # Task exists
+        task = Task.objects.get(id=task_id)
+        self.assertEqual(task.status, "Initiated")
+        self.assertEqual(task.details.get("model"), "Pattern")
+        self.assertEqual(task.details.get("id"), pattern.id)
+
+
 class PatternInstanceViewSetTest(SharedDataMixin, APITestCase):
     def test_pattern_instance_list_view(self):
         url = reverse("patterninstance-list")
